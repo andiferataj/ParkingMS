807727aeb43ff393a0fd652ecacb2640fd42f2ac
andiferataj <andi.fr33@gmail.com>
2026-02-16 20:44:11 +0100

    renamed on db

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "parking");
+$conn = new mysqli("localhost", "root", "", "ParkingMS");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
